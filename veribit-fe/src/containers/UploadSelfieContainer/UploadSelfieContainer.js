06f7a445d889bbaa850cf25eee080ce823e0f2da
Wian Koch <WianKoch@gmail.com>
2019-10-25 18:31:48 +0200

    Added img compression to Upload doc/selfie cont.

--- veribit-fe/src/containers/UploadSelfieContainer/UploadSelfieContainer.js
+++ veribit-fe/src/containers/UploadSelfieContainer/UploadSelfieContainer.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'; 
+import React, { PureComponent } from 'react';
 import { bindActionCreators } from 'redux';
 import { compose } from 'recompose';
 import { Icon, Row, Col, Button, Layout } from 'antd';
@@ -6,31 +6,58 @@ import logo from 'assets/img/logo.png';
 import { connectAuth, authActionCreators } from 'core';
 import { promisify } from '../../utilities';
 import UploadDocument from '../../components/UploadDocument/UploadDocument';
+import imageCompression from 'browser-image-compression';
 
-const { Content, Header} = Layout;
+const { Content, Header } = Layout;
 
 class UploadSelfieContainer extends PureComponent {
 
   constructor(props) {
     super(props);
     this.state = {
-      imgSrc: ''
+      imgSrc: '',
+      imgSrcCompressed: '',
+    }
+  }
+
+  async CompressImage(imgSrcbase64) {
+    try {
+      const imagefile = await imageCompression.getFilefromDataUrl(imgSrcbase64)
+
+      const options = {
+        maxSizeMB: 0.09,
+        maxWidthOrHeight: 2000,
+        useWebWorker: false
+      }
+
+      const compressedFile = await imageCompression(imagefile, options);
+      const compressedFilebase64 = await imageCompression.getDataUrlFromFile(compressedFile);
+      this.setState({ imgSrcCompressed: compressedFilebase64 })
+    } catch (error) {
+      console.log(error);
     }
   }
 
   showTakePhotoPage = () => {
-    this.props.history.push('take_photo', {uploadType: 'selfie'});
+    this.props.history.push('take_photo', { uploadType: 'selfie' });
   }
 
   onChooseImage = (imgSrc) => {
-    this.setState(...this.state, {imgSrc: imgSrc});
+    this.setState(...this.state, { imgSrc: imgSrc });
   }
 
-  uploadDocument = () => {
-    if (this.state.imgSrc !== '' && this.props.user) {
-      promisify(this.props.updateSelfie, { 
+  async uploadDocument() {
+    const imgSrcbase64 = this.state.imgSrc;
+    try {
+      await this.CompressImage(imgSrcbase64);
+    }
+    catch (error) {
+      console.log(error);
+    }
+    if (this.state.imgSrc !== '' && this.state.imgSrcCompressed !== '' && this.props.user) {
+      promisify(this.props.updateSelfie, {
         token: this.props.user.token,
-        selfie: this.state.imgSrc
+        selfie: this.state.imgSrcCompressed
       })
         .then((userInfo) => {
           this.props.history.push(`/signin/${this.props.user.token}`);
@@ -43,7 +70,7 @@ class UploadSelfieContainer extends PureComponent {
     this.props.history.goBack();
   }
 
-  render () {
+  render() {
     return (
       <div className="block">
         <Layout>
@@ -56,12 +83,12 @@ class UploadSelfieContainer extends PureComponent {
             <Content className="main">
               <Row className="validation_logo_area">
                 <Col span={14} offset={5}>
-                  <img alt="true" src={logo} className="logo"/>
+                  <img alt="true" src={logo} className="logo" />
                 </Col>
               </Row>
-              <Row  className="validation_title_area">
+              <Row className="validation_title_area">
                 <Col span={12} offset={6}>
-                    <span className="validation_choose_title">&ensp;Upload&ensp;Selfie</span>
+                  <span className="validation_choose_title">&ensp;Upload&ensp;Selfie</span>
                 </Col>
               </Row>
               <Row className="upload_area">
@@ -71,15 +98,15 @@ class UploadSelfieContainer extends PureComponent {
               </Row>
               <Row className="upload_btn_area">
                 <Col className="take_area" offset={4} span={8}>
-                  <Button className="take_btn" onClick={this.showTakePhotoPage}>Take a Picture<Icon style={{ fontSize: 16, color: '#ffffff'}} type="camera" /></Button>
+                  <Button className="take_btn" onClick={this.showTakePhotoPage}>Take a Picture<Icon style={{ fontSize: 16, color: '#ffffff' }} type="camera" /></Button>
                 </Col>
                 <Col className="preview_area" span={8}>
-                  <Button className="preview_btn">Preview<Icon style={{ fontSize: 16, color: '#ffffff'}} type="eye" /></Button>
+                  <Button className="preview_btn">Preview<Icon style={{ fontSize: 16, color: '#ffffff' }} type="eye" /></Button>
                 </Col>
               </Row>
               <Row>
                 <Col offset={4} span={16}>
-                  <Button className={this.state.imgSrc === '' ? "continue_btn" : "continue_enable_btn"} disabled={this.state.imgSrc === '' ? true : false} onClick={this.uploadDocument}>NEXT</Button>
+                  <Button className={this.state.imgSrc === '' ? "continue_btn" : "continue_enable_btn"} disabled={this.state.imgSrc === '' ? true : false} onClick={this.uploadDocument.bind(this)}>NEXT</Button>
                 </Col>
               </Row>
             </Content>
@@ -87,10 +114,10 @@ class UploadSelfieContainer extends PureComponent {
         </Layout>
       </div>
     );
-  }  
+  }
 }
 
-const mapStateToProps = ({auth}) => ({
+const mapStateToProps = ({ auth }) => ({
   user: auth.user,
 });
 
