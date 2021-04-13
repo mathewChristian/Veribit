4b2a7950c86f5890fdeed7383f4114516255dc91
Asynx <admin@asynx.io>
2021-04-13 01:50:24 +0100

    RunCukes tests

---
+++ exchange/exchange-core/src/test/java/exchange/tests/RunCukesDirectThroughputTests.java
@@ -0,0 +1,27 @@
+package exchange.tests;
+
+import exchange.core.common.config.PerformanceConfiguration;
+import exchange.tests.steps.OrderStepdefs;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber"}, strict = true)
+@Slf4j
+public class RunCukesDirectThroughputTests {
+
+    @BeforeClass
+    public static void beforeClass() {
+        OrderStepdefs.testPerformanceConfiguration = PerformanceConfiguration.throughputPerformanceBuilder().build();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        OrderStepdefs.testPerformanceConfiguration = null;
+    }
+
+}
